314d6c05da1004cecc4a3de00bcec88c70129af0
Subham Mishra <imsubham882@gmail.com>
2018-10-07 13:12:37 +0530

    Minor Changes

--- JAVASCRIPT file/script.js
+++ JAVASCRIPT file/script.js
@@ -1,11 +1,12 @@
-$(window).on("load", function () {
+$(window).on("load", function() {
     $(".se-pre-con").fadeOut("slow");
 });
 
 function myFunction() {
     alert("You need to be logged into your fb account before clicking this button to connect with me!!");
 }
-$(window).scroll(function () {
+
+$(window).scroll(function() {
     var height = $(window).scrollTop();
     if (height > 100) {
         $("#back2Top").fadeIn();
@@ -13,8 +14,9 @@ $(window).scroll(function () {
         $("#back2Top").fadeOut();
     }
 });
-$(document).ready(function () {
-    $("#back2Top").click(function (event) {
+
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
         event.preventDefault();
         $("html, body").animate({
             scrollTop: 0
@@ -22,54 +24,61 @@ $(document).ready(function () {
         return false;
     });
 });
-$(function () {
-    $(".scroll-0").click(function () {
+
+$(function() {
+    $(".scroll-0").click(function() {
         $("html, body").animate({
             scrollTop: $("div#top").offset().top
         }, "slow");
         return false;
     });
 });
-$(function () {
-    $(".scroll-1").click(function () {
+
+$(function() {
+    $(".scroll-1").click(function() {
         $("html, body").animate({
             scrollTop: $("div#certificates").offset().top
         }, "slow");
         return false;
     });
 });
-$(function () {
-    $(".scroll-2").click(function () {
+
+$(function() {
+    $(".scroll-2").click(function() {
         $("html, body").animate({
             scrollTop: $("div#aboutme").offset().top
         }, "slow");
         return false;
     });
 });
-$(function () {
-    $(".scroll-3").click(function () {
+
+$(function() {
+    $(".scroll-3").click(function() {
         $("html, body").animate({
             scrollTop: $("div.skills").offset().top
         }, "slow");
         return false;
     });
 });
-$(function () {
-    $(".scroll-4").click(function () {
+
+$(function() {
+    $(".scroll-4").click(function() {
         $("html, body").animate({
             scrollTop: $("div#contact").offset().top
         }, "slow");
         return false;
     });
 });
-$(function () {
-    $(".scroll-5").click(function () {
+
+$(function() {
+    $(".scroll-5").click(function() {
         $("html, body").animate({
             scrollTop: $("div.Template").offset().top
         }, "slow");
         return false;
     });
 });
+
 AOS.init({
     duration: 1200
 });
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Subham-Mishra.github.io
 My Self-Coded Personal Website ::
 This was the first website coded by me .I have made it so that it can act as my personal website which includes all my projects ,
-my contact details... I am open to all suggestions and improvements which can be made in any of my projects...
+my contact details and my memories... I am open to all suggestions and improvements which can be made in any of my projects...
 Thank You
